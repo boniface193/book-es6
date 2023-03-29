517a5bf96c63b68dc9b4500343dcf2ca682afd02
deboik193 <deboik193@gmail.com>
2023-03-29 08:57:15 +0100

    Re-create your Awesome books app

---
+++ modules/navigation.js
@@ -0,0 +1,28 @@
+const home = document.getElementById('list');
+const addNew = document.getElementById('addNew');
+const Contact = document.getElementById('Contact');
+const index = document.getElementById('index');
+const addBook = document.getElementById('addBook');
+const contact = document.getElementById('contact');
+
+const Navigator = () => {
+  home.addEventListener('click', () => {
+    addBook.classList.add('d-none');
+    contact.classList.add('d-none');
+    index.classList.remove('d-none');
+  });
+
+  addNew.addEventListener('click', () => {
+    index.classList.add('d-none');
+    contact.classList.add('d-none');
+    addBook.classList.remove('d-none');
+  });
+
+  Contact.addEventListener('click', () => {
+    index.classList.add('d-none');
+    addBook.classList.add('d-none');
+    contact.classList.remove('d-none');
+  });
+};
+
+export default Navigator;
---
+++ modules/showBook.js
@@ -0,0 +1,29 @@
+const showItem = document.getElementById('showItems');
+
+const showBook = () => {
+  showItem.innerHTML = '';
+  const getpayLoad = JSON.parse(localStorage.getItem('payLoad'));
+
+  if (getpayLoad !== null) {
+    getpayLoad.forEach((item, index) => {
+      showItem.innerHTML += `
+        <div class="d-flex justify-between mt-1 px-2 mb-1">
+          <p class="indent">"${item.title}" by ${item.author.toUpperCase()}</p>
+          <button type="submit" id="removeItem" btn_id=${index} class="card-strip removeItem">Remove</button>
+        </div>
+      `;
+    });
+
+    const removeItemBYClass = document.querySelectorAll('.removeItem');
+    const storage = JSON.parse(localStorage.getItem('payLoad'));
+    removeItemBYClass.forEach((rvBtn, index) => {
+      rvBtn.addEventListener('click', () => {
+        storage.splice(index, 1);
+        localStorage.setItem('payLoad', JSON.stringify(storage));
+        showBook();
+      });
+    });
+  }
+};
+
+export default showBook;
